da3f6e23d47875e4e73d501c34a6f2cbb1e13393
Waqar <mwaqar29@gmail.com>
2018-09-13 11:45:12 +0530

    Adding port as env var

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 
-app.listen(3000, () => {
-	console.log('App Running Successfully!');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App Running Successfully on port ${process.env.PORT}`);
 });
 
 /*
